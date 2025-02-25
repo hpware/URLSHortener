17802d2757291252aa20dd0a9b55b0e839a1a3f2
吳元皓 <hpware3@gmail.com>
2025-02-25 09:53:03 +0800

    Refactor API structure and update static assets

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,14 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  srcDir: "server",
-  compatibilityDate: "2025-02-25"
+  srcDir: "app",
+  compatibilityDate: "2025-02-25",
+  static: true,
+  publicAssets: [
+    {
+      baseURL: "static",
+      dir: "public/static",
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    },
+  ],
+  serveStatic: true,
 });
