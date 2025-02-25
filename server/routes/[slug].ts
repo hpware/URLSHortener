7a83a8584bc814942ae4e1624b483a05b96e41ae
吳元皓 <hpware3@gmail.com>
2025-02-25 09:04:14 +0800

    Init

---
+++ server/routes/[slug].ts
@@ -0,0 +1,6 @@
+export default defineEventHandler(async (event) => {
+    const slug = getRouterParam(event, "slug");
+    return {
+        slug: slug
+    }
+})
---
+++ server/routes/[slugtype]/[slug].ts
@@ -0,0 +1,8 @@
+export default defineEventHandler(async (event) => {
+    const slugtype = getRouterParam(event, "slugtype");
+    const slug = getRouterParam(event, "slug");
+    return {
+        slugtype: slugtype,
+        slug: slug
+    }
+})
