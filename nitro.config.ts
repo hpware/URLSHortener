9cd20ee0aaf2d280e1374263802bd8efa64ae390
吳元皓 <hpware3@gmail.com>
2025-02-25 12:31:09 +0800

    Remove unused static assets and simplify configuration

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,13 +2,4 @@
 export default defineNitroConfig({
   srcDir: "app",
   compatibilityDate: "2025-02-25",
-  static: true,
-  publicAssets: [
-    {
-      baseURL: "static",
-      dir: "public/static",
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-    },
-  ],
-  serveStatic: true,
 });
