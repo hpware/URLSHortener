f8af61f83c2cf9c546ccf7d62c879dd5aab4cfd8
吳元皓 <hpware3@gmail.com>
2025-02-25 15:31:47 +0800

    Add database configuration for SQLite and PostgreSQL

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,4 +2,17 @@
 export default defineNitroConfig({
   srcDir: "app",
   compatibilityDate: "2025-02-25",
+  experimental: {
+    database: true
+  },
+  database: {
+    default: {
+      connector: "sqlite",
+      options: {name: "db"}
+    },
+    users: {
+      connector: 'postgresql',
+      url: process.env.postgres,
+    }
+  }
 });
