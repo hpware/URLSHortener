56e0d5ae73d2ffba3515a803e4c09adad0b270a9
吳元皓 <hpware3@gmail.com>
2025-03-17 00:14:12 +0800

    Add initial REST API setup with Nitro configuration and Docker support

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd ./management-panel
-docker build -t shortener-management-panel-url .
-cd ..
 cd ./shortener
 docker build -t shortener-main-shortener .
 cd ..
+cd ./rest-api
+docker build -t shortener-rest-api .
+cd ..
+cd ./management-panel
+docker build -t shortener-management-panel-url .
