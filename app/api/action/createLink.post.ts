004f17bd88d4e1a8054cf4c848a77ce3df5ae608
元皓yh <hpware3@gmail.com>
2025-03-21 10:06:23 +0000

    Create APIs

--- app/api/action/createLink.post.ts
+++ app/api/action/createLink.post.ts
@@ -1,18 +1,38 @@
 // d is the short term for db or sql
 import d from "~/db";
 
+// Interface
+interface bodyi {
+  slug: string,
+  domain?: string
+  dest: string,
+  auth: string,
+}
 export default defineEventHandler(async (event) => {
     setHeader(event, "Content-Type", "application/json");
     const date = new Date();
     // Body using the letter b for well, idk.
-    const b = await readRawBody(event);
+    const b : Promise<bodyi> =  await readBody(event);
+    if (!b) {
+      return {
+        status: "error",
+        message: "You are required to enter slug, dest, and auth in the body."
+      }
+    }
     console.log(b);
-    if (!b.slug || !b.domain || !b.dest || !b.auth) {
+    if (!b.slug || !b.dest || !b.auth) {
         return {
           status: "error",
           message: "Missing required fields: slug, domain, dest, and auth are required"
         };
       }
+    if (!b.domain) {
+      const domainreq = await d`
+      `
+
+    } else {
+      const domain = b.domain;
+    }
     const checklink = d`
     
     `.execute();
