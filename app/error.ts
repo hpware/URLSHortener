465ea3a7e67aa04221c3d99f6dfe29a6d12999bc
吳元皓 <hpware3@gmail.com>
2025-03-23 22:21:26 +0800

    Enhance custom error handler in error.ts to provide a detailed 404 error page with event and error information for improved user experience

--- app/error.ts
+++ app/error.ts
@@ -1,4 +1,24 @@
 export default defineNitroErrorHandler(async (error, event) => {
-    setHeader(event, "Content-Type", "text/html");
-    return send(event, '[custom error handler] ' + error.stack)
+    setHeader(event, "Content-Type", "text/html")
+    const title = "Oops! 404"
+    const title2 = "This URL does not to appear to go anywhere"
+    const html = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>${title}</title>
+                <meta name=""
+            </head>
+            <body>
+                <h1>${title}</h1>
+                <h2>${title2}</h2>
+                <h4>Your event</h4>
+                <p>${JSON.stringify(event)}</p>
+                <h4>Your error message</h4>
+                <p>${error.stack}</p>
+                <button>Submit your problem to your web admin.</button>
+            </body>
+        </html>
+    `
+    return send(event, html);
 })
